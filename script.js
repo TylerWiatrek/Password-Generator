fd00df78a0cb6f5cf02aeba27e9380c44d0f3cc8
Tyler Wiatrek <wiatrektyler@yahoo.com>
2021-11-11 22:20:45 -0600

    adding character limits

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //Getting the characters response from the user
+  var characters = window.prompt("How many characters would you like the password to have?");
+  //Making sure the user entered at least 8 characters
+  if (characters < 8) {
+    window.alert("You need to have at least 8 characters. Please try again.");
+    return writePassword();
+  }
+  //Making sure the user entered no more than 128 characters
+  else if (characters > 128) {
+    window.alert("You cannot have more than 128 characters. Please try again.");
+    return writePassword();
+  }
+  else {
+    window.alert("Youre awesome.");
+  }
+  
+  
   var password = generatePassword();
-  window.alert("Hello");
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
